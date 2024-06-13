cbe209c2f7cbdca72736a8678ed0da25a8fe92d6
Zamyr Guevara <blinkx2003@gmail.com>
2024-06-12 21:09:26 -0500

    Cambios al sidebar y borrado de elementos innecesarios

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -52,7 +52,7 @@
     <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
     <link rel="icon" type="image/png" href="../assets/img/favicon.png">
     <title>
-        Corporate UI by Creative Tim & UPDIVISION
+        UEPS
     </title>
     <!--     Fonts and icons     -->
     <link
