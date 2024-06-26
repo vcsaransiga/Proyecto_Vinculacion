e36ae79fbc67ee059b1af87c40c1a792c420e187
Alejox102004 <116172949+Alejox102004@users.noreply.github.com>
2024-06-25 22:39:37 -0500

    Acerca de Nosotros Listo

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,14 +1,32 @@
 <x-app-layout>
+    <x-app.navbar />
+
     <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg">
         <div class="container-fluid py-6">
             <div class="row">
                 <div class="col-12">
                     <div class="card mb-4">
-                        <div class="card-header pb-0">
-                            <h6>About</h6>
+                        <div class="card-header pb-0 text-aligne-center">
+                            <h4>Sobre Nosotros</h4>
                         </div>
                         <div class="card-body">
-                            <p>Ini adalah halaman about</p>
+                            <h5 style="text-align: center;">Unidad Educativa de Produccion "SARANCE"</h5>
+                            <h6>Misión de la Institución Educativa:</h6>
+                            <p style="text-align: justify">La Unidad Educativa “SARANCE” es una Institución Fiscal de excelencia, de carácter técnico y humanístico, orientada a formar estudiantes con espíritu crítico y emprendedor, aplicando procesos psicopedagógicos actualizados, concordantes con la ciencia y tecnología de vanguardia, como herramienta para la solución de problemas sociales y ambientales en un mundo globalizado.</p>
+                            <h6>Visión de la Institución Educativa:</h6>
+                            <p style="text-align: justify">Formar bachilleres técnicos en Producción Agropecuaria e Industrialización de Productos Alimenticios y Ciencias, legando a la sociedad educandos que contribuyan a mejorar la calidad de vida de la comunidad, la conservación y el manejo responsable de los recursos naturales con un equipo de técnicos, docentes y directivos que ofrecen aprendizajes de calidad y calidez con un nivel ético y académico de excelencia comprometido e inclusivo.</p>
+                            <div class="">
+                                <img src="../assets/img/logoUEP.png" alt="ues" style="width: 400px; height: 400px; margin: 0 auto;">
+                            </div>
+                            
+                            <h5 style="text-align: center;">UEP-UES</h5>
+                            <h6>Misión de la UEP-UES</h6>
+                            <p style="text-align: justify">Organizar y orientar la formación integral permanente de los estudiantes vinculándolos en lo teórico- practico en los procesos de producción, investigación y administración, contribuyendo con una alimentación saludable de la comunidad educativa y su entorno y a la vez formar profesionales competentes, éticos para su desenvolvimiento en su vida profesional.</p>
+                            <h6>Visión de la UEP-UES</h6>
+                            <p style="text-align: justify" >Dentro de  4 años seremos una Unidad de Producción líder en la zona 1 en las áreas de Agropecuaria y Agroindustrias, llegando a ser Autosustentable y Autosostenible y referencia principal en el país.</p>
+                            <div class="">
+                                <img src="../assets/img/logohorizontal.png" alt="ues" style="width: 500px; height: 400px; margin: 0 auto;">
+                            </div>
                         </div>
                     </div>
                 </div>
--- resources/views/components/app/sidebar.blade.php
+++ resources/views/components/app/sidebar.blade.php
@@ -190,21 +190,22 @@
                     <div
                         class="icon icon-shape icon-sm px-0 text-center d-flex align-items-center justify-content-center">
                         <svg width="30px" height="30px" viewBox="0 0 48 48" version="1.1"
-                            xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                            <title>Acerca De</title>
-                            <g id="wallet" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                <g id="credit-card" transform="translate(12.000000, 15.000000)" fill="#FFFFFF">
-                                    <path class="color-background"
-                                        d="M3,0 C1.343145,0 0,1.343145 0,3 L0,4.5 L24,4.5 L24,3 C24,1.343145 22.6569,0 21,0 L3,0 Z"
-                                        id="Path" fill-rule="nonzero"></path>
-                                    <path class="color-foreground"
-                                        d="M24,7.5 L0,7.5 L0,15 C0,16.6569 1.343145,18 3,18 L21,18 C22.6569,18 24,16.6569 24,15 L24,7.5 Z M3,13.5 C3,12.67155 3.67158,12 4.5,12 L6,12 C6.82842,12 7.5,12.67155 7.5,13.5 C7.5,14.32845 6.82842,15 6,15 L4.5,15 C3.67158,15 3,14.32845 3,13.5 Z M10.5,12 C9.67158,12 9,12.67155 9,13.5 C9,14.32845 9.67158,15 10.5,15 L12,15 C12.82845,15 13.5,14.32845 13.5,13.5 C13.5,12.67155 12.82845,12 12,12 L10.5,12 Z"
-                                        id="Shape"></path>
-                                </g>
+                        xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                        <title>Sobre Nosotros</title>
+                        <g id="book-icon" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                            <g id="open-book" transform="translate(12.000000, 15.000000)" fill="#FFFFFF">
+                                <path class="color-background"
+                                    d="M0,0 L24,0 C24,8 12,8 12,16 C12,8 0,8 0,0 Z"
+                                    id="Path" fill-rule="nonzero"></path>
+                                <path class="color-foreground"
+                                    d="M0,18 L24,18 C24,10 12,10 12,2 C12,10 0,10 0,18 Z"
+                                    id="Shape"></path>
                             </g>
-                        </svg>
+                        </g>
+                    </svg>
+                    
                     </div>
-                    <span class="nav-link-text ms-1">Acerca De</span>
+                    <span class="nav-link-text ms-1">Sobre Nosotros</span>
                 </a>
             </li>
             {{-- <li class="nav-item mt-2">
