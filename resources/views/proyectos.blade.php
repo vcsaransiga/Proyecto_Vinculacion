1833de3a6fa41f0cc5d9dbf3a24c4788cd57f477
Alejox102004 <116172949+Alejox102004@users.noreply.github.com>
2024-06-28 20:42:45 -0500

    Agregar Vista detalles proyecto

--- resources/views/proyectos.blade.php
+++ resources/views/proyectos.blade.php
@@ -1,5 +1,34 @@
 <x-app-layout>
+    <x-app.navbar />
+
     <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg">
-        
+        <div class="container-fluid py-6">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card mb-4">
+                        <div class="card-header pb-0">
+                            <h3 style="margin-left: 5vh;">Datos Generales</h3>
+                        </div>
+                        <div  style="display: flex">
+                            <div style="margin-top: 7vh; margin-left: 7vh;">
+                                <p>Nombre del Proyecto:</p>
+                                <p>Responsable del Proyecto:</p>
+                                <p>Periodo del Proyecto:</p>
+                                <p>Presupuesto del proyecto:</p>
+                            </div>
+                            <div style="margin-top: 7vh; margin-left: 5vh;">
+                                <p>Proyecto de Producción de Hortalizas</p>
+                                <p>Ing. Juan Perez</p>
+                                <p>2021-2022</p>
+                                <p>$ 1000</p>
+                            </div>
+                            <div class="col-4" style="background-color:rgb(143, 143, 143); border-radius:2%; margin-rigth=0">
+
+                            </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -107,6 +107,10 @@ Route::get('/about', function () {
     return view('about');
 })->name('about');
 
+Route::get('/proyectos', function () {
+    return view('proyectos');
+})->name('proyectos');
+
 
 // Route::get('/laravel-examples/user-profile', [ProfileController::class, 'index'])->name('users.profile')->middleware('auth');
 // Route::put('/laravel-examples/user-profile/update', [ProfileController::class, 'update'])->name('users.update')->middleware('auth');
