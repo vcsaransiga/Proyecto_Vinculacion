dc0d211368a322a78c18dbefde0601a0e79b8d1a
Zamyr Guevara <blinkx2003@gmail.com>
2024-07-14 16:25:59 -0500

    Cambios en el dashboard (Tareas Recientes)

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,14 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Project;
+use App\Models\Task;
 
 class DashboardController extends Controller
 {
     public function index()
     {
         // Obtén solo seis proyectos
+        $tasks = Task::limit(6)->get();
         $projects = Project::limit(6)->get();
         // Pasa los proyectos a la vista del dashboard
-        return view('dashboard', compact('projects'));
+        return view('dashboard', compact('projects'), compact('tasks'));
     }
 }
