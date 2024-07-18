b8d5f07377fba7e9a3f8e675154e993576dc5287
Zamyr Guevara <blinkx2003@gmail.com>
2024-07-17 21:26:39 -0500

    Dashboard Actualizado
    Grafico limitado a 5 proyectos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -187,9 +187,9 @@ use App\Models\Student;
                                 <script>
                                     document.addEventListener('DOMContentLoaded', function() {
                                         var ctx = document.getElementById('projectProgressChart').getContext('2d');
-                                        var projectNames = @json($projects->pluck('name'));
-                                        var projectProgress = @json($projects->pluck('progress'));
-                            
+                                        var projectNames = @json($projects->pluck('name')).slice(0, 5);
+                                        var projectProgress = @json($projects->pluck('progress')).slice(0, 5);
+                                    
                                         var chart = new Chart(ctx, {
                                             type: 'bar', // Puedes cambiar el tipo de gráfico aquí
                                             data: {
@@ -212,8 +212,8 @@ use App\Models\Student;
                                             }
                                         });
                                     });
-                                </script>                            </div>
-                            {{-- <button class="btn btn-white mb-0 ms-auto">View report</button> --}}
+                                    </script>
+                                                               </div>
                         </div>
                     </div>
                 </div>
@@ -225,49 +225,9 @@ use App\Models\Student;
                                     <h6 class="font-weight-semibold text-lg mb-0">Tareas Recientes</h6>
                                     <p class="text-sm mb-sm-0 mb-2">Estas son las tareas recientes</p>
                                 </div>
-                                {{-- <div class="ms-auto d-flex">
-                                    <button type="button" class="btn btn-sm btn-white mb-0 me-2">
-                                        View report
-                                    </button>
-                                    <button type="button"
-                                        class="btn btn-sm btn-dark btn-icon d-flex align-items-center mb-0">
-                                        <span class="btn-inner--icon">
-                                            <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg"
-                                                fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                                stroke="currentColor" class="d-block me-2">
-                                                <path stroke-linecap="round" stroke-linejoin="round"
-                                                    d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
-                                            </svg>
-                                        </span>
-                                        <span class="btn-inner--text">Download</span>
-                                    </button>
-                                </div> --}}
+                                
                             </div>
-                            {{-- <div class="pb-3 d-sm-flex align-items-center">
-                                <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-                                    <input type="radio" class="btn-check" name="btnradiotable" id="btnradiotable1"
-                                        autocomplete="off" checked>
-                                    <label class="btn btn-white px-3 mb-0" for="btnradiotable1">Todas</label>
-                                    <input type="radio" class="btn-check" name="btnradiotable" id="btnradiotable2"
-                                        autocomplete="off">
-                                    <label class="btn btn-white px-3 mb-0" for="btnradiotable2">Completadas</label>
-                                    <input type="radio" class="btn-check" name="btnradiotable" id="btnradiotable3"
-                                        autocomplete="off">
-                                    <label class="btn btn-white px-3 mb-0" for="btnradiotable3">Pendientes</label>
-                                </div>
-                                <div class="input-group w-sm-25 ms-auto">
-                                    <span class="input-group-text text-body">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px"
-                                            fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                            stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round"
-                                                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z">
-                                            </path>
-                                        </svg>
-                                    </span>
-                                    <input type="text" class="form-control" placeholder="Search">
-                                </div>
-                            </div> --}}
+                           
                         </div>
                         <div class="card-body px-0 py-0">
                             <div class="table-responsive p-0">
@@ -282,9 +242,6 @@ use App\Models\Student;
                                             </th>
                                             <th class="text-secondary text-xs font-weight-semibold opacity-7 ps-2">
                                                 Proyecto</th>
-                                            {{-- <th
-                                                class="text-center text-secondary text-xs font-weight-semibold opacity-7">
-                                            </th> --}}
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -308,14 +265,7 @@ use App\Models\Student;
                                                         <p class="text-dark text-sm mb-0">{{ $task->project->name }}</p>
                                                     </div>
                                                 </td>
-                                                {{-- <td class="align-middle">
-                                                    <a href="javascript:;" class="text-secondary font-weight-bold text-xs"
-                                                        data-bs-toggle="tooltip" data-bs-title="Editar Tarea">
-                                                        <svg width="14" height="14" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                            <path d="M11.2201 2.02495C10.8292 1.63482 10.196 1.63545 9.80585 2.02636C9.41572 2.41727 9.41635 3.05044 9.80726 3.44057L11.2201 2.02495ZM12.5572 6.18502C12.9481 6.57516 13.5813 6.57453 13.9714 6.18362C14.3615 5.79271 14.3609 5.15954 13.97 4.7694L12.5572 6.18502ZM11.6803 1.56839L12.3867 2.2762L12.3867 2.27619L11.6803 1.56839ZM14.4302 4.31284L15.1367 5.02065L15.1367 5.02064L14.4302 4.31284ZM3.72198 15V16C3.98686 16 4.24091 15.8949 4.42839 15.7078L3.72198 15ZM0.999756 15H-0.000244141C-0.000244141 15.5523 0.447471 16 0.999756 16L0.999756 15ZM0.999756 12.2279L0.293346 11.5201C0.105383 11.7077 -0.000244141 11.9624 -0.000244141 12.2279H0.999756ZM9.80726 3.44057L12.5572 6.18502L13.97 4.7694L11.2201 2.02495L9.80726 3.44057ZM12.3867 2.27619C12.7557 1.90794 13.3549 1.90794 13.7238 2.27619L15.1367 0.860593C13.9869 -0.286864 12.1236 -0.286864 10.9739 0.860593L12.3867 2.27619ZM13.7238 2.27619C14.0917 2.64337 14.0917 3.23787 13.7238 3.60504L15.1367 5.02064C16.2875 3.8721 16.2875 2.00913 15.1367 0.860593L13.7238 2.27619ZM13.7238 3.60504L3.01557 14.2922L4.42839 15.7078L15.1367 5.02065L13.7238 3.60504ZM3.72198 14H0.999756V16H3.72198V14ZM1.99976 15V12.2279H-0.000244141V15H1.99976ZM1.70617 12.9357L12.3867 2.2762L10.9739 0.86059L0.293346 11.5201L1.70617 12.9357Z" fill="#64748B"/>
-                                                        </svg>
-                                                    </a>
-                                                </td> --}}
+                                                
                                             </tr>
                                         @endforeach
                                     </tbody>
@@ -324,46 +274,7 @@ use App\Models\Student;
                         </div>
                     </div>
                 </div>
-            </div>
-
-            {{-- <div class="row">
-                <div class="col-lg-12">
-                    <div class="card shadow-xs border">
-                        <div class="card-header pb-0">
-                            <div class="d-sm-flex align-items-center mb-3">
-                                <div>
-                                    <h6 class="font-weight-semibold text-lg mb-0">Overview balance</h6>
-                                    <p class="text-sm mb-sm-0 mb-2">Here you have details about the balance.</p>
-                                </div>
-                                <div class="ms-auto d-flex">
-                                    <button type="button" class="btn btn-sm btn-white mb-0 me-2">
-                                        View report
-                                    </button>
-                                </div>
-                            </div>
-                            <div class="d-sm-flex align-items-center">
-                                <h3 class="mb-0 font-weight-semibold">$87,982.80</h3>
-                                <span
-                                    class="badge badge-sm border border-success text-success bg-success border-radius-sm ms-sm-3 px-2">
-                                    <svg width="9" height="9" viewBox="0 0 10 9" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                            d="M0.46967 4.46967C0.176777 4.76256 0.176777 5.23744 0.46967 5.53033C0.762563 5.82322 1.23744 5.82322 1.53033 5.53033L0.46967 4.46967ZM5.53033 1.53033C5.82322 1.23744 5.82322 0.762563 5.53033 0.46967C5.23744 0.176777 4.76256 0.176777 4.46967 0.46967L5.53033 1.53033ZM5.53033 0.46967C5.23744 0.176777 4.76256 0.176777 4.46967 0.46967C4.17678 0.762563 4.17678 1.23744 4.46967 1.53033L5.53033 0.46967ZM8.46967 5.53033C8.76256 5.82322 9.23744 5.82322 9.53033 5.53033C9.82322 5.23744 9.82322 4.76256 9.53033 4.46967L8.46967 5.53033ZM1.53033 5.53033L5.53033 1.53033L4.46967 0.46967L0.46967 4.46967L1.53033 5.53033ZM4.46967 1.53033L8.46967 5.53033L9.53033 4.46967L5.53033 0.46967L4.46967 1.53033Z"
-                                            fill="#67C23A"></path>
-                                    </svg>
-                                    10.5%
-                                </span>
-                            </div>
-                        </div>
-                        <div class="card-body p-3">
-                            <div class="chart mt-n6">
-                                <canvas id="chart-line" class="chart-canvas" height="300"></canvas>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> --}}
+            </div> 
     </main>
 
     <script src="{{ asset('js/breadcrumbs.js') }}"></script>
