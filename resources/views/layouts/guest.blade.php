c4780c0e36a22cd260eee95dabbe52c83b4ce59e
Diret03 <diegodavidrecalde@gmail.com>
2024-07-19 08:41:58 -0500

    scrollbar restaurado

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -73,7 +73,7 @@
     <!--   Core JS Files   -->
     <script src="../assets/js/core/popper.min.js"></script>
     <script src="../assets/js/core/bootstrap.min.js"></script>
-    {{-- <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script> --}}
+    <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
     <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
     <script>
         var win = navigator.platform.indexOf('Win') > -1;
